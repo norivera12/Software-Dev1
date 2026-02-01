0fdb798561d0166fc4a94bbd751b80ca6e871d8e
norivera12 <norivera12@gmail.com>
2026-02-01 13:25:41 -0500

    Create hw.1.java

---
+++ Assignment-one/hw.1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Debug {
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+//finish this one for me by receiving and printing the user's age back to them
+  System.out.println("How old are you? ");
+  Sring input = sc.nextLine();
+    System.out.println(input);
+//can you do some math for me? take these variables and print out the sum of them!
+//int num1 = 10;
+//int num2 = 37;
+//?
+}
+}
